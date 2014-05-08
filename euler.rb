5b139a11190dd05ba001efd89a05928c0887f3b1
William Wong <willwong430@gmail.com>
2014-05-08 15:07:12 -0400

    euler solutions

---
+++ euler.rb
@@ -0,0 +1,98 @@
+# Problem 1: Add all the natural numbers below 1000
+# that are multiples of 3 or 5. 
+
+def problem_1
+	0.upto(999).reduce do |sum, num|
+		num % 3 == 0 || num % 5 == 0 ? sum + num : sum
+	end
+end
+
+# problem_1 ==> 233168
+
+# <----------------------------->
+
+# Problem 2: For terms in the Fibonacci sequence
+# whose values do not exceed four million, 
+# find the sum of the even-valued terms.
+
+def problem_2
+	prev = 1
+	current = 1 
+	sum = 0
+	next_up = 0
+
+	while current < 4000000
+		next_up = prev + current
+		sum += next_up if next_up.even?
+		prev = current
+		current = next_up
+	end
+
+	sum
+end
+
+# problem_2 ==> 4613732
+
+# <----------------------------->
+
+# Problem 3: Largest prime factor of 600851475143.
+
+def problem_3
+	def factors_for(num)
+		return [] if num == 1
+		factor = (2..num).find { |x| num % x == 0 }
+		[factor] + factors_for(num / factor)
+	end
+
+	factors_for(600_851_475_143).max
+end
+
+# problem_3 ==> 6857
+
+# <----------------------------->
+
+# Problem 4: The largest palindrome made from the 
+# product of two 3-digit numbers.
+
+def problem_4
+	def palindrome?(n)
+		n.to_s == n.to_s.reverse
+	end
+
+	max = 0 
+	100.upto(999).each do |a|
+		a.upto(999).each do |b|
+			max = a * b if palindrome?(a * b) && (a * b) > max
+		end
+	end
+	max
+end
+
+# problem_4 ==> 906609
+
+# <----------------------------->
+
+# Problem 5: The smallest positive number that is 
+# evenly divisible by all of the numbers from 1 to 20.
+
+require 'rational'
+
+def problem_5
+	(1..20).inject(1) { |res, n| res.lcm n }
+end
+
+# problem_5 ==> 232792560
+
+# <----------------------------->
+
+
+
+
+
+
+
+
+
+
+
+
